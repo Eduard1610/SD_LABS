9e8c1eacb10716a72ded5912e1d3451d6a3bd55a
Eduard1610 <edurando@unsa.edu.pe>
2024-05-02 17:06:04 -0500

    Comentarios sobre el código del ejemplo THREAD

--- src/EjemploThread.java
+++ src/EjemploThread.java
@@ -1,3 +1,28 @@
+public class EjemploThread {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Cliente 1", new int[] {
+            2,
+            2,
+            1,
+            5,
+            2,
+            3
+        });
+        Cliente cliente2 = new Cliente("Cliente 2", new int[] {
+            1,
+            3,
+            5,
+            1,
+            1
+        });
+        //Tiempo inicial de referencia
+        long initialTime = System.currentTimeMillis();
+        CajeraThread cajera1 = new CajeraThread("Cajera 1", cliente1, initialTime);
+        CajeraThread cajera2 = new CajeraThread("Cajera 2", cliente2, initialTime);
+        cajera1.start();
+        cajera2.start();
+    }
+}
 class CajeraThread extends Thread {
     private String nombre;
     private Cliente cliente;
@@ -27,31 +52,28 @@ class CajeraThread extends Thread {
         this.cliente = cliente;
     }
     @Override
+
     public void run() {
-        System.out.println("La cajera " + this.nombre + " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE "
+        // Imprime un mensaje al inicio indicando que la cajera ha comenzado a procesar la compra del cliente
+        System.out.println("La cajera " + this.nombre + " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE " +
+                this.cliente.getNombre() + " EN EL TIEMPO: " +
+                (System.currentTimeMillis() - this.initialTime) / 1000 + "seg");
 
-            +
-            this.cliente.getNombre() + " EN EL TIEMPO: " +
-            (System.currentTimeMillis() - this.initialTime) / 1000 +
-            "seg");
-
+        // Itera sobre los productos en el carro de compra del cliente
         for (int i = 0; i < this.cliente.getCarroCompra().length; i++) {
-            // Se procesa el pedido en X segundos
+            // Se procesa el pedido en X segundos (simulado)
             this.esperarXsegundos(cliente.getCarroCompra()[i]);
-            System.out.println("Procesado el producto " + (i + 1)
-
-                +
-                " del cliente " + this.cliente.getNombre() + "->Tiempo: " +
-                (System.currentTimeMillis() - this.initialTime) / 1000 +
-                "seg");
-
+            System.out.println("Procesado el producto " + (i + 1) +
+                    " del cliente " + this.cliente.getNombre() + "->Tiempo: " +
+                    (System.currentTimeMillis() - this.initialTime) / 1000 + "seg");
         }
-        System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR " +
-            this.cliente.getNombre() + " EN EL TIEMPO: " +
-            (System.currentTimeMillis() - this.initialTime) / 1000 +
-            "seg");
 
+        // Imprime un mensaje al final indicando que la cajera ha terminado de procesar al cliente
+        System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR " +
+                this.cliente.getNombre() + " EN EL TIEMPO: " +
+                (System.currentTimeMillis() - this.initialTime) / 1000 + "seg");
     }
+
     private void esperarXsegundos(int segundos) {
         try {
             Thread.sleep(segundos * 1000);
@@ -79,30 +101,5 @@ class Cliente {
     }
     public void setCarroCompra(int[] carroCompra) {
         this.carroCompra = carroCompra;
-    }
-}
-public class EjemploThread {
-    public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Cliente 1", new int[] {
-            2,
-            2,
-            1,
-            5,
-            2,
-            3
-        });
-        Cliente cliente2 = new Cliente("Cliente 2", new int[] {
-            1,
-            3,
-            5,
-            1,
-            1
-        });
-        //Tiempo inicial de referencia
-        long initialTime = System.currentTimeMillis();
-        CajeraThread cajera1 = new CajeraThread("Cajera 1", cliente1, initialTime);
-        CajeraThread cajera2 = new CajeraThread("Cajera 2", cliente2, initialTime);
-        cajera1.start();
-        cajera2.start();
     }
 }
