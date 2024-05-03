7d0bed8d79bb9b87caf697e2e3c81e4747a7bd15
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-05-03 11:20:16 -0500

    Comentarios cajera.java

--- src/EjemploSimple.java
+++ src/EjemploSimple.java
@@ -1,19 +1,10 @@
 public class EjemploSimple {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("Cliente 1", new int[] {
-            2,
-            2,
-            1,
-            5,
-            2,
-            3
+            2, 2, 1, 5, 2, 3
         });
         Cliente cliente2 = new Cliente("Cliente 2", new int[] {
-            1,
-            3,
-            5,
-            1,
-            1
+            1, 3, 5, 1, 1
         });
         cajera cajera1 = new cajera("Cajera 1");
         cajera cajera2 = new cajera("Cajera 2");
--- src/cajera.java
+++ src/cajera.java
@@ -1,15 +1,20 @@
+// Definicion de la clase cajera
 public class cajera {
     private String nombre;
     public cajera() {}
     public cajera(String nombre) {
         this.nombre = nombre;
     }
+    
+    // metodos getter y setter
     public String getNombre() {
         return nombre;
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    
+    // Muestra el tiempo que tarda en procesar la compra de un cliente
     public void procesarCompra(Cliente cliente, long timeStamp) {
         System.out.println("La cajera " + this.nombre +
 
@@ -30,6 +35,8 @@ public class cajera {
             (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
 
     }
+
+    // Metodo para pausar la ejecucion del programa durante x segundos
     private void esperarXsegundos(int segundos) {
         try {
             Thread.sleep(segundos * 1000);
