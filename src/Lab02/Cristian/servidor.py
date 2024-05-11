0b5a0221e2349bef333bd7fd2823c5f63a5ad1e4
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-05-10 22:48:22 -0500

    Cristian: socket escucha

--- src/Lab02/Cristian/servidor.py
+++ src/Lab02/Cristian/servidor.py
@@ -3,6 +3,15 @@ import datetime
 
 # funcion para iniciar el servidor de reloj
 def iniciar_servidor():
-    print("Iniciando servidor de reloj")
+    
+    s = socket.socket()
+    print("Socket creado exitosamente!")
+
+    puerto = 8000
+    s.bind(('', puerto))
+
+    # escuchar las solicitudes
+    s.listen(5)
+    print("Socket escuchando en el puerto ", puerto)
 
 # funcion principal
