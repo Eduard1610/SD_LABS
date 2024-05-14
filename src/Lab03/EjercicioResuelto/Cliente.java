2be1fb2ca6e1875491ce47333ebedf96ea890e1f
ivelita <ivelita@unsa.edu.pe>
2024-05-14 10:31:39 -0500

    Agregando analisis de lineas al lab3 Ejercicio resuelto

--- src/Lab03/EjercicioResuelto/Cliente.java
+++ src/Lab03/EjercicioResuelto/Cliente.java
@@ -6,11 +6,11 @@ public class Cliente {
     static final int PUERTO = 5000;
     public Cliente() {
         try {
-            Socket skCliente = new Socket(HOST, PUERTO);
-            InputStream aux = skCliente.getInputStream();
-            DataInputStream flujo = new DataInputStream(aux);
-            System.out.println(flujo.readUTF());
-            skCliente.close();
+            Socket skCliente = new Socket(HOST, PUERTO); //Nuevo socket del cliente que conecta con el servidor
+            InputStream aux = skCliente.getInputStream(); //Obtener el flujo de entrada del cliente
+            DataInputStream flujo = new DataInputStream(aux); //Obtener el flujo de datos del cliente
+            System.out.println(flujo.readUTF()); //Leer data bajo estandar utf
+            skCliente.close(); //Cierra conexion con cliente
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
--- src/Lab03/EjercicioResuelto/Servidor.java
+++ src/Lab03/EjercicioResuelto/Servidor.java
@@ -5,15 +5,15 @@ public class Servidor {
     static final int PUERTO = 5000;
     public Servidor() {
         try {
-            ServerSocket skServidor = new ServerSocket(PUERTO);
-            System.out.println("Escucho el puerto " + PUERTO);
-            for (int numCli = 0; numCli < 3; numCli++) {
+            ServerSocket skServidor = new ServerSocket(PUERTO); //Crea socket para alojar el servidor
+            System.out.println("Escucho el puerto " + PUERTO); //Imprime confirmacion
+            for (int numCli = 0; numCli < 3; numCli++) { //Bucle para n clientes
                 Socket skCliente = skServidor.accept(); // Crea objeto
-                System.out.println("Sirvo al cliente " + numCli);
-                OutputStream aux = skCliente.getOutputStream();
-                DataOutputStream flujo = new DataOutputStream(aux);
-                flujo.writeUTF("Hola cliente " + numCli);
-                skCliente.close();
+                System.out.println("Sirvo al cliente " + numCli); //Lado del server, lista el cliente conectado
+                OutputStream aux = skCliente.getOutputStream(); //Optiene el flujo de salida del cliente
+                DataOutputStream flujo = new DataOutputStream(aux); //Crea el flujo de datos para el cliente
+                flujo.writeUTF("Hola cliente " + numCli); //Escribe en el flujo de salida
+                skCliente.close(); //Cierra conexion con cliente
             }
             System.out.println("Demasiados clientes por hoy");
         } catch (Exception e) {
