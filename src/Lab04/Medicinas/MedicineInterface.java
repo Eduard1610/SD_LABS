239dc0b7384a4b5fe2185c5437b659e8c1d9cb6a
ivelita <ivelita@unsa.edu.pe>
2024-05-16 09:40:23 -0500

    Agregando Ejercicio Propuesto 1

---
+++ src/Lab04/Medicinas/MedicineInterface.java
@@ -0,0 +1,7 @@
+package Lab04.Medicinas;
+import java.rmi.Remote;
+public interface MedicineInterface extends Remote {
+    public Medicine getMedicine(int amount) throws Exception;
+    public int getStock() throws Exception;
+    public String print() throws Exception;
+}
